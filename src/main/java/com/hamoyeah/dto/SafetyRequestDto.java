675311ddec93162d20f9dd689399040439e92d82
junhyunsim0402 <simjjang11@gmail.com>
2026-03-20 17:41:29 +0900

    feat 1. dto 세팅

--- src/main/java/com/hamoyeah/dto/SafetyRequestDto.java
+++ src/main/java/com/hamoyeah/dto/SafetyRequestDto.java
@@ -1,4 +1,10 @@
 package com.hamoyeah.dto;
 
+import lombok.Data;
+
+@Data
 public class SafetyRequestDto {
+    private double lat;      // 사용자 위도
+    private double lng;      // 사용자 경도
+    private int radius;      // 반경 (미터)
 }
