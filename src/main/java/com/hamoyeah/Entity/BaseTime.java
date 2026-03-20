1afb1d2f386bd9bd59d90188618625c4e1375e63
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-20 18:00:02 +0900

    feat: BaseTime 생성

---
+++ src/main/java/com/hamoyeah/Entity/BaseTime.java
@@ -0,0 +1,13 @@
+package com.hamoyeah.Entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTime {
+
+}
