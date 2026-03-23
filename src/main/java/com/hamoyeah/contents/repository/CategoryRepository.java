eec6606bb7cb101916f82b12a57be948406131e3
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-23 16:25:57 +0900

    feat: 리포지토리 작성

--- src/main/java/com/hamoyeah/contents/repository/CategoryRepository.java
+++ src/main/java/com/hamoyeah/contents/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.hamoyeah.contents.repository;
 
-import com.hamoyeah.contents.Entity.ContentsEntity;
+import com.hamoyeah.contents.Entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContentsRepository extends JpaRepository<ContentsEntity, Integer> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
 }
