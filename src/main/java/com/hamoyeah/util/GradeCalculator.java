4472f5f3d3a53540dee3504deb56b3ca23be268a
junhyunsim0402 <simjjang11@gmail.com>
2026-03-26 12:49:13 +0900

    feat 1. 점수 로직 수정

--- src/main/java/com/hamoyeah/util/GradeCalculator.java
+++ src/main/java/com/hamoyeah/util/GradeCalculator.java
@@ -15,14 +15,15 @@ public class GradeCalculator {
 
         if (totalPlus > 0) {
             // CCTV는 3점 가중치, 가로등은 1점 가중치로 계산하는 로직 유지
-            double convertedTotal = cctvCount + (lampCount / 3.0);
-            cctvScore = (int) ((cctvCount / convertedTotal) * 60);
-            streetLampScore = (int) (((lampCount / 3.0) / convertedTotal) * 60);
+            cctvScore = cctvCount >= 40 ? 45 : cctvCount >= 30 ? 33 :
+                    cctvCount >= 22 ? 25 : cctvCount >= 15 ? 5 : 0;
+            streetLampScore = lampCount >= 130 ? 15 : lampCount >= 100 ? 10 :
+                    lampCount >= 60 ? 5 : lampCount >= 30 ? 2 : 0;
         }
 
         Map<String, Object> result = new HashMap<>();
-        result.put("cctvScore", Math.min(cctvScore, 45)); // 최대치 방어
-        result.put("streetLampScore", Math.min(streetLampScore, 15));
+        result.put("cctvScore", cctvScore);
+        result.put("streetLampScore", streetLampScore);
         System.out.println("cctvCount = " + cctvCount);
         System.out.println("lampCount = " + lampCount);
         System.out.println("PlusResult = " + result);
@@ -30,9 +31,8 @@ public class GradeCalculator {
     }
     public Map<String,Object> minusScore(double noiseAvg,int pm10, int pm25){
          Map<String,Object> minusResult=new HashMap<>();
-         int noiseScore=noiseAvg >= 100 ? 20 : noiseAvg >= 90 ? 18 :
-                 noiseAvg >= 80 ? 16 : noiseAvg >= 70 ? 13 :
-                         noiseAvg >= 60 ? 10 : noiseAvg >= 40 ? 5 : 0;  // 소음점수
+         int noiseScore=noiseAvg >= 80 ? 20 : noiseAvg >= 60 ? 15 :
+                 noiseAvg >= 55 ? 10 : noiseAvg >= 50 ? 5 : 0 ;  // 소음점수
          int pm10Score = pm10 <= 30 ? 0 : pm10 <= 80 ? 5 : pm10 <= 150 ? 8 : 10;  // 환경부 공식 기준(미세먼지)
         int pm25Score = pm25 <= 15 ? 0 : pm25 <= 35 ? 5 : pm25 <= 75 ? 8 : 10;  // 초미세먼지
          int minusAirScore=pm10Score+pm25Score;
@@ -46,8 +46,8 @@ public class GradeCalculator {
 
     public Map<String,Object> totalGrade(int cctvScore, int streetLampScore, int noiseScore, int airScore){
          Map<String,Object> result=new HashMap<>();
-        int totalScore=( cctvScore + streetLampScore ) - ( noiseScore + airScore );
-        String grade = totalScore >= 80 ? "A" : totalScore >= 60 ? "B" : totalScore >= 40 ? "C" : "D";
+        int totalScore=( cctvScore + streetLampScore ) - ( noiseScore + airScore ); // + 점수 최대 : 60, - 점수 최대 : 40
+        String grade = totalScore >= 45 ? "A" : totalScore >= 30 ? "B" : totalScore >= 15 ? "C" : "D";
         result.put("totalScore",totalScore); result.put("grade",grade);
         System.out.println("grade = " + grade);
         return result;
