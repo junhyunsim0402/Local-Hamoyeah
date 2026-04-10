1b2cb3e27de14b5e1d6f0bde540d974edc851c15
yexxjx <chenstyle02@gmail.com>
2026-04-10 18:19:23 +0900

    feat 설명서

---
+++ src/main/Local-React/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="manual-page">
+      <div className="start-ment">
+        <p className="first-ment">진주를 더 재밌게 즐기는 법</p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Banner;
